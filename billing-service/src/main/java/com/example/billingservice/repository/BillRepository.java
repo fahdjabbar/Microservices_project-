6e143f8a345c3d174f45e5f6fe538afcd3b516f2
fahd <jabbarfahd67@gmail.com>
2025-11-17 19:20:09 +0100

    Ajout de billing-service et configuration openfeign

---
+++ billing-service/src/main/java/com/example/billingservice/repository/BillRepository.java
@@ -0,0 +1,9 @@
+package com.example.billingservice.repository;
+
+import com.example.billingservice.entities.Bill;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  BillRepository extends JpaRepository<Bill, Long> {
+
+}
